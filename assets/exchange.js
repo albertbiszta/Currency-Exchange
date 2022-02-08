f177b95f34e955f54251bae45da753f642d0a516
Albert Biszta <albertbiszta@gmail.com>
2022-02-08 16:40:24 +0100

    changed form styles

--- assets/exchange.js
+++ assets/exchange.js
@@ -4,18 +4,24 @@ class Exchange {
         this.amountField = document.getElementById(`${formName}_amount`);
         this.primaryCurrency = document.getElementById(`${formName}_primaryCurrency`);
         this.targetCurrency = document.getElementById(`${formName}_targetCurrency`);
-        this.amountField.addEventListener('change', () => {
-            fetch('/public/get-conversion-result', {
-                method: 'POST',
-                body: JSON.stringify({'primaryCurrency': this.primaryCurrency.value, 'targetCurrency': this.targetCurrency.value, amount: parseInt(this.amountField.value)})
-            })
-                .then(result => result.json())
-                .then(result => console.log(result));
-        });
+        this.amountField.addEventListener('change', () => isNaN(this.amountField.value) ? alert('Amount must be a number!') : this.showResult());
+        this.primaryCurrency.addEventListener('change', () => this.showResult());
+        this.targetCurrency.addEventListener('change', () => this.showResult());
     }
 
-    showConversionResult() {
-        console.log(this.primaryCurrency.value);
+    showResult() {
+        fetch('/public/get-conversion-result', {
+            method: 'POST',
+            body: JSON.stringify({'primaryCurrency': this.primaryCurrency.value, 'targetCurrency': this.targetCurrency.value, amount: parseInt(this.amountField.value)})
+        })
+            .then(result => result.json())
+            .then(result => {
+                const alert = document.querySelector('div.alert-primary');
+                alert.innerHTML = `${this.amountField.value} ${this.primaryCurrency.value} = ${result} ${this.targetCurrency.value}`;
+                alert.hidden = false;
+            });
     }
+
+
 }
 const exchange = new Exchange();
