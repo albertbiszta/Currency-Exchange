c0458248122260d8f1026fd237da38f9fe7d61d6
Albert Biszta <albertbiszta@gmail.com>
2022-11-13 17:09:12 +0100

    Update CurrencyFormType.php

--- src/Form/CurrencyFormType.php
+++ src/Form/CurrencyFormType.php
@@ -2,15 +2,9 @@
 
 namespace App\Form;
 
-use App\Entity\Exchange;
 use App\Enum\Currency;
 use JetBrains\PhpStorm\ArrayShape;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EnumType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 abstract class CurrencyFormType extends AbstractType
 {
